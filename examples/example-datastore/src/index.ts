314f89b601ed5db5b04873aeb93dee5df5c508b1
Ian Rose <ian.r.rose@gmail.com>
2019-06-03 16:59:05 -0700

    Add sliders.

--- examples/example-datastore/src/index.ts
+++ examples/example-datastore/src/index.ts
@@ -13,7 +13,7 @@ import { IMessageHandler, Message, MessageLoop } from '@phosphor/messaging';
 class InMemoryPatchStore implements IMessageHandler {
   createDatastore(): Datastore {
     const id = this._id++;
-    const ds = Datastore.create({ id , schemas: [schema], broadcastHandler: this});
+    const ds = Datastore.create({ id , schemas: [slideschema, checkschema], broadcastHandler: this});
     this._stores.set(id, ds);
     return ds;
   }
@@ -38,16 +38,23 @@ class InMemoryPatchStore implements IMessageHandler {
   private _stores = new Map<number, Datastore>();
 }
 
-const schema = {
+const checkschema = {
   id: 'checkboxes',
   fields: {
     value: Fields.Boolean({ value: false })
   }
 };
 
-function setValue(ds: Datastore, value: boolean) {
+const slideschema = {
+  id: 'sliders',
+  fields: {
+    value: Fields.Number({ value: 0 })
+  }
+};
+
+function setCheckValue(ds: Datastore, value: boolean) {
   ds.beginTransaction();
-  ds.get(schema).update({
+  ds.get(checkschema).update({
     box: { value: value }
   });
   ds.endTransaction();
@@ -56,32 +63,69 @@ function setValue(ds: Datastore, value: boolean) {
 function createCheckbox(ds: Datastore, value: boolean) {
   const checkbox: HTMLInputElement = document.createElement('input');
   checkbox.type = 'checkbox';
-  checkbox.checked = ds.get(schema).get('box')!.value;
+  checkbox.checked = ds.get(checkschema).get('box')!.value;
   checkbox.oninput = evt => {
-    setValue(ds, checkbox.checked);
+    setCheckValue(ds, checkbox.checked);
   };
   return checkbox;
 }
 
+function setSlideValue(ds: Datastore, value: number) {
+  ds.beginTransaction();
+  ds.get(slideschema).update({
+    slide: { value: value }
+  });
+  ds.endTransaction();
+}
+
+function createSlider(ds: Datastore, value: number) {
+  const slider: HTMLInputElement = document.createElement('input');
+  slider.type = 'range';
+  slider.min = '0';
+  slider.max = '10';
+  slider.step = '1';
+  slider.value = `${ds.get(slideschema).get('slide')!.value}`;
+  slider.oninput = evt => {
+    setSlideValue(ds, parseInt(slider.value, 10));
+  };
+  return slider;
+}
 
 function main(): void {
   const store = new InMemoryPatchStore();
 
   const ds1 = store.createDatastore();
   const ds2 = store.createDatastore();
-  setValue(ds1, true);
-  setValue(ds2, false);
+  setCheckValue(ds1, true);
+  setCheckValue(ds2, false);
+  setSlideValue(ds1, 10);
+  setSlideValue(ds2, 0);
 
-  ds1.changed.connect((_, change: any) => {
-    console.log(change.change.checkboxes.box.value);
-    if (checkbox1.checked !== change.change.checkboxes.box.value.current) {
-      checkbox1.checked = change.change.checkboxes.box.value.current;
+  ds1.changed.connect((_, c: any) => {
+    const change = c.change;
+    if (change.checkboxes) {
+      if (checkbox1.checked !== change.checkboxes.box.value.current) {
+        checkbox1.checked = change.checkboxes.box.value.current;
+      }
+    }
+    if (change.sliders) {
+      if (slide1.value !== change.sliders.slide.value.current) {
+        slide1.value = change.sliders.slide.value.current;
+      }
     }
   });
-  ds2.changed.connect((_, change: any) => {
-    console.log(change.change.checkboxes.box.value);
-    if (checkbox2.checked !== change.change.checkboxes.box.value.current) {
-      checkbox2.checked = change.change.checkboxes.box.value.current;
+
+  ds2.changed.connect((_, c: any) => {
+    const change = c.change;
+    if (change.checkboxes) {
+      if (checkbox2.checked !== change.checkboxes.box.value.current) {
+        checkbox2.checked = change.checkboxes.box.value.current;
+      }
+    }
+    if (change.sliders) {
+      if (slide2.value !== change.sliders.slide.value.current) {
+        slide2.value = change.sliders.slide.value.current;
+      }
     }
   });
 
@@ -89,6 +133,10 @@ function main(): void {
   const checkbox2 = createCheckbox(ds2, true);
   document.body.appendChild(checkbox1);
   document.body.appendChild(checkbox2);
+  const slide1 = createSlider(ds1, 0);
+  const slide2 = createSlider(ds2, 10);
+  document.body.appendChild(slide1);
+  document.body.appendChild(slide2);
 }
 
 window.onload = main;
